13f920c1ad1e7fbffc6cf7ea9215cb4a5040ea3d
eyvmal <eyvind.malde@gmail.com>
2024-09-23 12:03:08 +0200

    core and extension

--- src/extensions/extensions.js
+++ src/extensions/extensions.js
@@ -4,6 +4,11 @@
 // The function must return "Phil's cake is ready!" if the remaining minutes is 0,
 // "The cake is still baking!" if there are any remaining minutes left,
 // or "You didn't set a timer!" if no value is provided to the parameter
+function timerStatus(timer) {
+  if (timer <= 0) return "Phil's cake is ready!"
+  else if (typeof timer === 'undefined') return "You didn't set a timer!"
+  else return 'The cake is still baking!'
+}
 
 // 2. To help Phil prepare ahead of time, create a function named estimatePrepTime
 // that accepts two parameters:
@@ -13,6 +18,10 @@
 // number of ingredients provided and the prep time per ingredient.
 // If no prep time per ingredient is provided, the function should assume each ingredient
 // takes 2 minutes to prepare
+function estimatePrepTime(ingredients, prepTime) {
+  if (typeof prepTime === 'undefined') prepTime = 2
+  return ingredients.length * prepTime
+}
 
 // 3. Phil needs to know the quantity of milk and eggs to use! Create a function
 // named calculateQuantities which accepts two parameters:
@@ -29,7 +38,12 @@
 //
 // calculateQuantities(["milk", "eggs"], 3)
 // returns: { sugar: 0, eggs: 6 }
-
+function calculateQuantities(ingredients, layers) {
+  const shoppingList = {}
+  shoppingList.sugar = ingredients.includes('sugar') ? 100 * layers : 0
+  shoppingList.eggs = ingredients.includes('eggs') ? 2 * layers : 0
+  return shoppingList
+}
 // 4. Phil's cake is pretty small and only provides 1 portion. He wants to make a bigger one!
 // Create a function named improveRecipe that accepts two parameters:
 // - an object where the keys are ingredients and the values are quantities
@@ -42,6 +56,12 @@
 // Example:
 // improveRecipe({ eggs: 2, milk: 100, sugar: 200 }, 3)
 // returns: { eggs: 6, milk: 300, sugar: 600 }
+function improveRecipe(recipe, portions) {
+  for (const ingredient in recipe) {
+    recipe[ingredient] *= portions
+  }
+  return recipe
+}
 
 // Don't change the code below this line
 module.exports = {
